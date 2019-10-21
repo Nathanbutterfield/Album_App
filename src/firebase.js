6fea9ad39df6e2b7304248b12e65c250153b8d70
Nathanbutterfield <54914767+Nathanbutterfield@users.noreply.github.com>
2019-10-21 14:37:25 -0600

    firebase and travis set up

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyD9nWDYPdxnDKr2VhKYopSDEiA7UNDDR2k",
-  authDomain: "todo-aa992.firebaseapp.com",
-  databaseURL: "https://todo-aa992.firebaseio.com",
-  projectId: "todo-aa992",
-  storageBucket: "todo-aa992.appspot.com",
-  messagingSenderId: "232429625722",
-  appId: "1:232429625722:web:92798181e1495c9638f6c3"
+  apiKey: "AIzaSyC6ae1f5Ja0mL8kwRR34H9S8H5EUxIRswk",
+  authDomain: "album-a01d3.firebaseapp.com",
+  databaseURL: "https://album-a01d3.firebaseio.com",
+  projectId: "album-a01d3",
+  storageBucket: "album-a01d3.appspot.com",
+  messagingSenderId: "730410207280",
+  appId: "1:730410207280:web:b96f8050f7c732262f45f5"
 };
 
 firebase.initializeApp(config);
